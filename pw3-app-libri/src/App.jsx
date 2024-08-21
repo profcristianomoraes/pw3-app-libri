9ca330775926929d9fadb8be710c7860260eed15
profcristianomoraes <cristianocorrea3@gmail.com>
2024-08-21 19:18:53 -0300

    Telas básica (home, criar livros e listar livros) e sistema de navegação

--- pw3-app-libri/src/App.jsx
+++ pw3-app-libri/src/App.jsx
@@ -1,25 +1,72 @@
+/* Importa os componentes de navegação */
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 /* Importa o arquivo base de CSS de App.jsx */
 import './App.css'
 
 /* Importa o compoente de BookCard */
-import BookCard from './components/BookCard'
+// import BookCard from './components/BookCard'
+
+/* Importa o compoente de NavBar */
+import NavBar from './components/layout/NavBar'
+
+/* Importa o compoente de Container */
+import Container from './components/layout/Container'
+
+/* Importa o componente de página de HOME */
+import Home from './components/pages/Home'
+
+/* Importa o componente de página de CREATE BOOKS */
+import Createbooks from './components/pages/Createbooks'
+
+/* Importa o componente de página de LIST BOOKS */
+import ListBooks from './components/pages/ListBooks'
+
 
 /* Objeto de imagem para teste de insercão de imagem */
-import cavernas from './assets/cavernas_aco.jpg'
+// import cavernas from './assets/cavernas_aco.jpg'
 
 function App() {
   return (
     <>
-      <div>
 
-        {/* Chamada do componente de BookCard com passagem de dados por meio de props */}
-        <BookCard 
-          titulo='As Cavernas de Aço'
-          autor='Isaac Azimov'
-          imagem={cavernas}
-        />
+      {/* Chamada do componente de BookCard com passagem de dados por meio de props */}
+      {/* <BookCard 
+        titulo='As Cavernas de Aço'
+        autor='Isaac Azimov'
+        imagem={cavernas}
+      /> */}
 
-      </div>
+      {/* Chamada do componente de NavBar */}
+      {/* <NavBar /> */}
+
+        {/* <img className='book_home' src="./book_home.jpg" alt="" /> */}
+        {/* <Home /> */}
+        {/* <Createbooks /> */}
+        {/* <ListBooks /> */}
+
+        {/* ESTRUTURA DE ROTAS/NAVEGAÇÃO */}
+        <BrowserRouter>
+
+        <Container>
+
+            <Routes>
+
+              <Route path='/' element={<NavBar />}>
+
+                  <Route path='/' element={<Home />} />
+                  <Route path='/listBook' element={<ListBooks />} />
+                  <Route path='/newBook' element={<Createbooks />} />
+
+              </Route>
+
+            </Routes>
+
+        </Container>
+
+        </BrowserRouter>
+
+
     </>
   )
 }
---
+++ pw3-app-libri/src/components/pages/Createbooks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from './Createbooks.module.css'
+
+const Createbooks = () => {
+    return (
+        <section className={style.create_book_container}>
+            <h1>CREATE BOOKS</h1>
+        </section>
+    )
+}
+
+export default Createbooks
